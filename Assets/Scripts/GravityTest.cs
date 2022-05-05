6fb31b28c30eb61559ae4dfd39b6fcce6d4be371
rech4210 <rech0421k@gmail.com>
2022-05-06 00:58:42 +0900

    중력테스트 수정 완료
    엘레멘트 이름 값 변경시킬것
    !~3 원통 4~6 구체

---
+++ Assets/Scripts/GravityTest.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct Mygravity
+{
+    public float mass;
+    public float drag;
+    public float angular;
+
+    public Mygravity(float mass, float drag, float angular)
+    {
+        this.mass = mass;
+        this.drag = drag;
+        this.angular = angular;
+    }
+
+}
+
+public class GravityTest : MonoBehaviour
+{
+    List<Rigidbody> rigidbody_list = new List<Rigidbody>();
+
+    public GameObject[] GravityTools;
+    public GameObject[] GravityBall;
+    
+    public Mygravity[] gravityfool = new Mygravity[6];
+
+    
+
+    void Start()
+    {
+        for (int i = 0; i < GravityTools.Length; i++)
+        {
+            rigidbody_list.Add(GravityTools[i].GetComponent<Rigidbody>());
+        }
+
+        for (int i = 0; i < GravityBall.Length; i++)
+        {
+            rigidbody_list.Add(GravityBall[i].GetComponent<Rigidbody>());
+        }
+
+        for (int i = 0; i < gravityfool.Length; i++)
+        {
+            
+            Mygravity mygravity = new Mygravity();
+            gravityfool[i] = mygravity;
+            gravityfool[i].mass = rigidbody_list[i].mass;
+            gravityfool[i].drag = rigidbody_list[i].drag;
+            gravityfool[i].angular = rigidbody_list[i].angularDrag;
+        }
+
+    }
+    private void Update()
+    {
+        for (int i = 0; i < GravityTools.Length; i++)
+        {
+            rigidbody_list[i].mass = gravityfool[i].mass;
+            rigidbody_list[i].drag = gravityfool[i].drag;
+            rigidbody_list[i].angularDrag = gravityfool[i].angular;
+        }
+
+        for (int i = 3; i < GravityBall.Length+3; i++)
+        {
+            rigidbody_list[i].mass = gravityfool[i].mass;
+            rigidbody_list[i].drag = gravityfool[i].drag;
+            rigidbody_list[i].angularDrag = gravityfool[i].angular;
+        }
+    }
+}
--- Assets/Scripts/RamdaExpression/Ramda.cs
+++ Assets/Scripts/RamdaExpression/Ramda.cs
@@ -7,8 +7,10 @@ public class Ramda : MonoBehaviour
     delegate void Find(string name);
     delegate string Kill(string name, int count);
 
-    string man = "��������";
-    string woman = "�ʹ��";
+    int killcount = 5;
+
+    string man = "가지구이";
+    string woman = "꽈배기";
 
     int health;
 
@@ -20,10 +22,10 @@ public class Ramda : MonoBehaviour
         find += FindWoman;
         Debug.Log(find);
 
-        find += (string findman) => { Debug.Log("Find" + findman); Debug.Log("�����" + findman); };// ���ٽ��� �̿��� ��������Ʈ �Ｎ ���� �Ű����� ���� ����
+        find += (string findman) => { Debug.Log("Find" + findman); Debug.Log("찾기" + findman); };// ���ٽ��� �̿��� ��������Ʈ �Ｎ ���� �Ű����� ���� ����
 
 
-        kill += (string killman, int count) => (killman + "��" + count + "ȸ óġ"); Debug.Log("�Ϸ�"); // �Ű������� �ΰ� �̻��� ��쿡 ����ϴ� ���ٽ�
+        kill += (string killman, int count) => (killman + "가" + count + "잡았습니다"); Debug.Log("만세"); // �Ű������� �ΰ� �̻��� ��쿡 ����ϴ� ���ٽ�
 
     }
 
@@ -32,19 +34,19 @@ public class Ramda : MonoBehaviour
         Debug.Log("Findwoman " + this.woman);
     }
 
-    public void RestoreHealth(int health) // ���� �޼ҵ�
+    public void RestoreHealth(int health) // 기존 메소드
     {
         this.health += health;
     }
-    public void RestoreHealth_ramda(int health) => this.health += health; //���� �޼ҵ�
+    public void RestoreHealth_ramda(int health) => this.health += health; //람다 메소드
 
-    public int hp // ���� ������Ƽ
+    public int hp // 기존 프로퍼티
     {
         get { return health; }
         set { health = 10; }
     }
 
-    public int hp_ramda { get => health; set => health = 10;} // ���� ������Ƽ
+    public int hp_ramda { get => health; set => health = 10;} //람다 프로퍼티
 
 
     private void Update()
